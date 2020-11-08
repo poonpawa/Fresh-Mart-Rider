035ee3b7777d41dadb7e976103e7e13c1ffee640
Sumit Narang <sumitnarang.web@gmail.com>
2020-11-08 15:43:20 +0000

    change css of order history and account

--- screens/orderHistory.js
+++ screens/orderHistory.js
@@ -16,24 +16,83 @@ const orderHistory = (props) => {
 
 
     return (
-        <ScrollView>
+        <View style={styles.container}>
+            <Text style={styles.fixHead}>Order history</Text>
+            <ScrollView>
+                <Text style={styles.head}>PREVIOUS ORDERS</Text>
+                <View>
+                    {
+                        orders.map((item, key) => (
 
-            <Text h4>Previous Orders</Text>
-            <View>
-                {
-                    orders.map((item, key) => (
-                        <ListItem key={key}
-                            title={'Order Id: ' + item.id}
-                            subtitle={'Store: ' + item.store + '\nAmount Paid : €' + item.totalPrice}
-                            rightSubtitle={<Button title='View Details' />}
-                            bottomDivider />
-                    ))
-                }
-            </View>
-        </ScrollView>
+                            <View key={key} style={styles.previousContainer}>
+                                <View style={styles.eachOrder}>
+                                    <Text style={styles.firstTitle}>Order Id: {item.id}</Text>
+                                    <Text style={styles.secondTitle}>Store: {item.store}</Text>
+                                    <Text style={styles.lastTitle}>Amount Paid: € {item.totalPrice}</Text>
+                                </View>
+                            </View>
+
+                        ))
+                    }
+                </View>
+            </ScrollView>
+        </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#FAFAFA',
+        height: '100%'
+    },
+    fixHead: {
+        color: '#383F51',
+        textAlign: 'center',
+        fontFamily: "NunitoSans-Bold",
+        fontSize: 22,
+        height: 60,
+        lineHeight: 60,
+        borderBottomColor: '#F3F3F3',
+        borderBottomWidth: 1,
+        backgroundColor: 'white'
+    },
+    previousContainer: {
+        backgroundColor: 'white',
+        paddingLeft: 16,
+        paddingRight: 16
+    },
+    head: {
+        color: '#6A748A',
+        fontFamily: "NunitoSans-SemiBold",
+        fontSize: 14,
+        marginTop: 12,
+        marginBottom: 12,
+        paddingLeft: 16,
+        paddingRight: 16
+    },
+    firstTitle: {
+        color: '#6A748A',
+        fontFamily: "NunitoSans-SemiBold",
+        fontSize: 15,
+        marginTop: 12,
+    },
+    secondTitle: {
+        color: '#6A748A',
+        fontFamily: "NunitoSans-SemiBold",
+        fontSize: 15,
+        marginTop: 8
+    },
+    lastTitle: {
+        color: '#6A748A',
+        fontFamily: "NunitoSans-SemiBold",
+        fontSize: 15,
+        marginTop: 8,
+        marginBottom: 16,
+    },
+    eachOrder: {
+        borderBottomColor: '#ECECF6',
+        borderBottomWidth: 1,   
+    }
+})
 
 export default orderHistory
